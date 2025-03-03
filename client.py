7936ef905cde6599b8c2b428bb4f7309b7d601db
dodsonta <tdodson@pdx.edu>
2025-03-03 14:06:37 -0800

    Basics Continuing

--- client.py
+++ client.py
@@ -1,40 +1,60 @@
 import socket
 import sys
+import threading
 
 class client:
     def __init__(self, username):
         self.username = username
     
-    def connectToServer(username):
-        serverAddress = ('localhost', 5000)
-        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clientSocket.connect(serverAddress)
-        clientSocket.sendall(f"Hello from user {username}".encode('utf-8'))
-        return clientSocket
+    def connectToServer(self):
+        serverAddress = ('localhost', 6667)
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clientSocket.connect(serverAddress)
+        self.clientSocket.sendall(f"{self.username}".encode('utf-8'))
     
-    def sendMessage(clientSocket, message):
-        clientSocket.sendall(message.encode('utf-8'))
-
-    def receiveMessage(clientSocket):
+    def sendMessage(self):
         while True:
-            data = clientSocket.recv(1024)
-            print(f"Received: {data.decode('utf-8')}")
+            message = input("Enter message: ")
+            if(message.lower == "exit" or message.lower == "quit"):
+                self.clientSocket.sendall(message.encode('utf-8'))
+                break
+            self.clientSocket.sendall(message.encode('utf-8'))
+        self.clientSocket.close()
 
-    
+    def receiveMessage(self):
+        while True:
+            try:
+                data = self.clientSocket.recv(1024)
+                if data:
+                    msg = data.decode('utf-8')
+                    if msg == "Server is shutting down":
+                        print("Server is shutting down. Ending connection")
+                        self.clientSocket.close()
+                        sys.exit(0)
+                    print(f"Received: {msg}")
+            except ConnectionResetError:
+                break
 
 if __name__ == "__main__":
     if(len(sys.argv) != 2):
+        print("Error: requires a username as an argument")
         exit(1)
     username = sys.argv[1]
-    clientSocket = client.connectToServer(username)
+    client = client(username)
+    client.connectToServer()
 
     print(f"Connected to server with username: {username}")
+    sendThread = threading.Thread(target=client.sendMessage)
+    receiveThread = threading.Thread(target=client.receiveMessage)
 
-    while True: 
-        message = input("Enter message: ")
-        if(message.lower == "exit" or message.lower == "quit"):
-            break
-        client.sendMessage(clientSocket, message)
-        client.receiveMessage(clientSocket)
+    sendThread.start()
+    receiveThread.start()
+    sendThread.join()
+    receiveThread.join()
 
-    clientSocket.close()
+    # while True: 
+    #     message = input("Enter message: ")
+    #     if(message.lower == "exit" or message.lower == "quit"):
+    #         break
+    #     client.sendMessage(clientSocket, message)
+    #     client.receiveMessage(clientSocket)
--- server.py
+++ server.py
@@ -1,32 +1,68 @@
 import socket
 import threading
+import sys
+import signal
+import time 
 
 clients = {}
 rooms = {}
 
-def handleClient(clientSocket, clientAddress):
-    username = clientSocket.recv(1024).decode('utf-8')
-    clients[username] = clientSocket
-    print(f"Connected with user named: {username}")
-    while True:
-        message = clientSocket.recv(1024).decode('utf-8')
-        print(f"Received message from {username}: {message}")
-        if(message.lower() == "exit" or message.lower() == "quit"):
-            break
-        for user in clients:
-            if user != username:
-                clients[user].sendall(f"{username}: {message}".encode('utf-8'))
+class Server:
+    def __init__(self, host='localhost', port=6667):
+        self.serverAddr = (host, port)
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serverSocket.settimeout(1)
+        self.clients = {}
+        self.rooms = {}
+        self.online = True
 
-def startServer():
-    serverAddr = ('localhost', 6667)
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.bind(serverAddr)
-    serverSocket.listen(5)
-    print("Server started and listening on port 6667")
-    while True:
-        clientSocket, clientAddr = serverSocket.accept()
-        clientThread = threading.Thread(target=handleClient, args=(clientSocket, clientAddr))
-        clientThread.start()
+    def startServer(self):
+        self.serverSocket.bind(self.serverAddr)
+        self.serverSocket.listen(5)
+        serverAddr = ('localhost', 6667)
+        print("Server started and listening on port 6667")
+        signal.signal(signal.SIGINT, self.signal_handler)
+        while self.online:
+            try:
+                clientSocket, clientAddr = self.serverSocket.accept()
+                clientThread = threading.Thread(target=self.handleClient, args=(clientSocket, clientAddr))
+                clientThread.start()
+            except socket.timeout:
+                continue
+
+    def handleClient(self, clientSocket, clientAddress):
+        username = clientSocket.recv(1024).decode('utf-8')
+        self.clients[username] = clientSocket
+        print(f"Connected with user named: {username}")
+        clientSocket.settimeout(1)
+        while self.online:
+            try:
+                message = clientSocket.recv(1024).decode('utf-8')
+                print(f"Received message from {username}: {message}")
+                if(message.lower() == "exit" or message.lower() == "quit"):
+                    break
+                for user in self.clients:
+                    if user != username:
+                        self.clients[user].sendall(f"{username}: {message}".encode('utf-8'))
+            except socket.timeout:
+                continue
+            except ConnectionResetError:
+                break 
+        clientSocket.close()
+        del self.clients[username]
+        print(f"{username}  disconnected")
+
+    def signal_handler(self, sig, frame):
+        print("Exiting server")
+        self.online = False
+        time.sleep(5)
+        for user in self.clients:
+            self.clients[user].sendall("Server is shutting down".encode('utf-8'))
+            self.clients[user].close()
+        if self.serverSocket:
+            self.serverSocket.close()
+        sys.exit(0)
 
 if __name__ == "__main__":
-    startServer()
+    server = Server()
+    server.startServer()
